7c9f635de751a91258e8aa3c0fe990634fedf312
VasuBhalani <vasubhalani2004@gmail.com>
2025-07-12 13:03:14 +0530

    create the api for the produvts of all and user related products

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -2,7 +2,7 @@ import Product from '../models/Products.js';
 import { v4 as uuidv4 } from 'uuid';
 
 const generateProductId = () => {
-  return `prod-${uuidv4().split('-')[0].slice(0, 6).toLowerCase()}`;
+  return `PROD_${uuidv4().split('-')[0].slice(0, 6).toLowerCase()}`;
 };
 
 export const registerProduct = async (req, res) => {
@@ -24,9 +24,9 @@ export const registerProduct = async (req, res) => {
       availability
     } = req.body;
 
-    const owner = req.user._id; // 👈 Get from authMiddleware
+    const owner = req.user._id; 
 
-    const product_id = `PROD-${uuidv4().slice(0, 6).toUpperCase()}`;
+    const product_id = generateProductId();
 
     const newProduct = new Product({
       product_id,
@@ -58,6 +58,77 @@ export const registerProduct = async (req, res) => {
     res.status(500).json({
       success: false,
       message: 'Server error while registering product'
+    });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      success: true,
+      products
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server error while fetching products'
+    });
+  }
+};
+
+export const getProductById = async (req, res) => {
+  console.log('Fetching product with ID:', req.params.id);
+  try {
+    const product = await Product.findOne({ product_id: req.params.id });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: 'Product not found'
+      });
+    }
+    res.status(200).json({
+      success: true,
+      product
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server error while fetching product'
+    });
+  }
+};
+
+export const getUserProducts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    console.log('Fetching products for user ID:', userId);
+    const products = await Product.find({ owner: userId });
+    res.status(200).json({
+      success: true,
+      products
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server error while fetching user products'
+    });
+  }
+};
+
+export const getProductsByCategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+    res.status(200).json({
+      success: true,
+      products
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Server error while fetching products by category'
     });
   }
 };
